6a88cba5fa0cc9acce57e40087503d6a4f9a9989
huainiannin <huainiannin@gmail.com>
2018-02-11 11:44:02 +0800

    提交文件

--- play.sh
+++ play.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-echo helloworld
+#mysql connect info
+host = 'localhost'
+port = '3306'
+user = 'root'
+passwd = 'root'
+database = 'music'
+table = 'music'
+music = $1
+music = awk -F '/' 'print{$4}'
+music = awk -F '-' 'print{$1}'
+music = awk -F '-' 'print{$2}'
+sql = 'SELECT COUNT(1) FROM MUSIC WHERE MUSIC'
+#get mysql connect 
+#insert one record into database
+#begin count time
+#begin play music
+#end count time
+#get mysql connect 
+#insert on record into database
+exit
