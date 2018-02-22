75e521c4543f2dec98fbcdfd110c3b9190295e60
huainiannin <huainiannin@gmail.com>
2018-02-22 11:13:41 +0800

    音乐播放流程梳理，计时

--- play.sh
+++ play.sh
@@ -8,14 +8,26 @@ database = 'music'
 table = 'music'
 music = $1
 fullName = awk -F '/' 'print{$NF}'
-music = awk -F '-' 'print{$1}'
+album = awk -F '-' 'print{$1}'
 singer = awk -F '-' 'print{$2}'
 sql = 'SELECT COUNT(1) FROM MUSIC WHERE MUSIC'
 #get mysql connect 
+
+#check if this album exist in database if exist album count ++
+#if not exit 
 #insert one record into database
+
+#check if this singer exist in database if exist singer count ++
+#if not exist insert one record into database
+
 #begin count time
+begin=`date +%s`
 #begin play music
+mplayer music
 #end count time
+end=`date +%s`
+duration=`expr $end - $begin`
+
 #get mysql connect 
-#insert on record into database
+#update album and singer record
 exit
