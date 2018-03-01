7137e5ae665b23ca26e9f5233acf0fd60ea2a9cf
huainiannin <huainiannin@gmail.com>
2018-03-01 16:46:02 +0800

    提交可用版本

--- play.sh
+++ play.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 #mysql connect info
-host = 'localhost'
-port = '3306'
-user = 'root'
-passwd = 'root'
-database = 'music'
-table = 'music'
-music = $1
-fullName = awk -F '~' '{print$2}' 
-album = awk -F '-' 'print{$1}'
-singer = awk -F '-' 'print{$2}'
-sql = 'SELECT COUNT(1) FROM MUSIC WHERE MUSIC'
-#get mysql connect 
-
-#check if this album exist in database if exist album count ++
-#if not exit 
-#insert one record into database
-
-#check if this singer exist in database if exist singer count ++
-#if not exist insert one record into database
-
+host='localhost'
+port='3306'
+user='root'
+passwd='root'
+database='music'
+table='music'
+music=$1
+fullName=`echo "${music}" | awk -F '~' '{print$2}'`
+album=`echo "${music}" | awk -F '-' '{print$1}' | cut -c 2-`
+singer=`echo "${music}" | awk -F '-' '{print$2}' | sed 's/~//g'`
 #begin count time
 begin=`date +%s`
 #begin play music
-mplayer music
+pid=`mplayer music &`
 #end count time
 end=`date +%s`
 duration=`expr $end - $begin`
 
-#get mysql connect 
-#update album and singer record
+sql="insert into music values ('${begin}','${album}','${singer}','${begin}','${duration}')"
+
+#execute sql
+mysql -h${host} -P${port} -u${user} -p${passwd} -D${database} -e "${sql}"
 exit
